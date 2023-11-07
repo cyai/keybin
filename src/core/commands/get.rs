ba645ce37b55ad5464215cfa8a33a64db2406785
cyai <83634399+cyai@users.noreply.github.com>
2023-11-07 21:22:54 +0530

    feat: added service to get secret from api call.

---
+++ src/core/commands/get.rs
@@ -0,0 +1,38 @@
+use reqwest;
+use serde_json::{json, Value};  
+use dotenv::dotenv;
+
+
+
+pub async fn get(_id: &str) -> Result<String, reqwest::Error> {
+    dotenv().ok();
+    let url = "https://vault.aws.us.pangea.cloud/v1/get";
+
+    let client = reqwest::Client::new();
+
+    let access_token = std::env::var("VAULT_ACCESS_TOKEN").expect("VAULT_ACCESS_TOKEN must be set");
+
+    println!("ID = {}", _id );
+    let payload = json!({
+        "id": _id
+    });
+
+    let response = client
+        .post(url)
+        .header("Authorization", format!("Bearer {}", access_token))
+        .header("Content-Type", "application/json")
+        .json(&payload)
+        .send()
+        .await?;
+
+
+    if response.status().is_success(){
+        let text = response.text().await?;
+        let text: Value = serde_json::from_str(&text).unwrap();
+        return Ok(text["result"]["current_version"]["secret"].to_string());
+    } else {    
+        println!("Response failed: {:?}", response.status());
+        return Ok(format!("Response failed: {:?}", response.status()));
+    }
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
+mod core {
+    pub mod commands {
+        pub mod get;
+    }
+}
+
+use core::commands::get::get;
+use dotenv::dotenv;
+use std::io;
+
+#[tokio::main]
+async fn main() {
+    let mut secret_id = String::new();
+    dotenv().ok();
+
     println!("Hello, world!");
+    println!("Enter secret id: ");
+    io::stdin().read_line(&mut secret_id).expect("Failed to read line");
+
+    let secret_id = secret_id.trim();
+
+    let result = get(&secret_id).await;
+    println!("{:?}", result);
+
 }
