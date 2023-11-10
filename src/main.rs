de84fe76b04e6f65f39351e26a4b25cb84ac1d41
cyai <83634399+cyai@users.noreply.github.com>
2023-11-11 03:38:48 +0530

    feat: added update service to udpate the keys and secrets

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,11 @@ mod core {
         pub mod get;
         pub mod list;
         pub mod store;
+        pub mod update;
     }
 }
 
-use core::commands::{get::get, list::list, store::store};
+use core::commands::{get::get, list::list, store::store, update::update};
 use dotenv::dotenv;
 use std::io;
 use serde_json::Value;
@@ -60,7 +61,7 @@ async fn main(){
         let mut secret = String::new();
         let mut name = String::new();
 
-        println!("Enter the name: ");
+        println!("Enter the name: ");   
         io::stdin().read_line(&mut secret).expect("Failed to read secret name");
         let secret = secret.trim();
 
@@ -71,6 +72,22 @@ async fn main(){
         let result = store(secret.to_string(), Some("secret".to_string()), Some(name.to_string()), Some("/personal".to_string()), None).await;
 
         println!("{:?}", result);
+    } else if service == "update" {
+        let mut secret_id = String::new();
+
+        println!("Enter the secret id: ");
+        io::stdin().read_line(&mut secret_id).expect("Failed to read secret id");
+        let secret_id = secret_id.trim();
+
+        let result = update(secret_id.to_string(), Some("secret-name".to_string()), Some("".to_string()), Some("".to_string()), Some("".to_string())).await;
+
+        if let Ok(json_result) = result{
+            let result: Value = serde_json::from_str(&json_result).unwrap();        
+            println!("Status: {:?}", result["status"].as_str().unwrap());
+            println!("Summary: {:?}", result["summary"].as_str().unwrap());
+        }
+    } else {
+        println!("Invalid service");
     }
  
     
